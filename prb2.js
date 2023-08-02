7ad7b8b993d9b92ad256e4de136782a79d43e29f
Afsana-Tasnim-Juha <111423065+Afsana-Tasnim-Juha@users.noreply.github.com>
2023-08-02 15:49:29 +0200

    prb2

---
+++ prb2.js
@@ -0,0 +1,10 @@
+var Mathematics = 75.25;
+var Biology = 65;
+var Chemistry = 80;
+var Physics = 35.45;
+var Bangla = 99.50;
+var totalMarks = Mathematics + Biology + Chemistry + Physics + Bangla;
+console.log(totalMarks);
+var avg = totalMarks / 5;
+var avgTwoDecimal = avg.toFixed(2);
+console.log(avgTwoDecimal);
