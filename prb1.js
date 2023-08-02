5b84f8fd239d3373548e7754b55c35544cfd3f5f
Afsana-Tasnim-Juha <111423065+Afsana-Tasnim-Juha@users.noreply.github.com>
2023-08-02 15:37:06 +0200

    first prb

---
+++ prb1.js
@@ -0,0 +1,8 @@
+var giveMoney = 1000;
+var applePrice = 400;
+var orangePrice = 300;
+
+var totalPrice = applePrice + orangePrice;
+console.log(totalPrice);
+var giveBack = giveMoney - totalPrice;
+console.log(giveBack);
